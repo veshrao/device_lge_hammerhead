bedb222e9834fb4346d8072e1bb7d53f6d494abf
Robert Shih <robertshih@google.com>
2019-11-25 22:25:42 -0800

    hammerhead: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device.mk
+++ device.mk
@@ -312,7 +312,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # FastCharge
 PRODUCT_PACKAGES += \
